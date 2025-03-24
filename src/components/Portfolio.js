c31431f8f52265d9cd62ae9857d009d4481c8e34
Ezinne Jane Duru <janeezyofficial@gmail.com>
2025-03-24 20:47:39 +0000

    updated links

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -302,7 +302,7 @@ const Portfolio = () => {
                   </motion.button>
 
                   <motion.a
-                    href="https://jane-dururesume.tiiny.site"
+                    href="https://salmon-kania-79.tiiny.site"
                     className={`
                       px-6 py-3 rounded-full text-lg font-semibold
                       border-2 border-blue-500
