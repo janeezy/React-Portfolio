dd992ac4c4d6068e516c369e060fc9b6f5bd9372
Jane Duru <51208612+janeezy@users.noreply.github.com>
2026-09-17 07:49:49 +0100

    Enable portfolio image and bookstore enhancements

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
 import PortfolioV2 from './components/PortfolioV2';
-function App(){return <div className="App"><PortfolioV2/></div>}
+import PortfolioEnhancements from './components/PortfolioEnhancements';
+function App(){return <div className="App"><PortfolioV2/><PortfolioEnhancements/></div>}
 export default App;
